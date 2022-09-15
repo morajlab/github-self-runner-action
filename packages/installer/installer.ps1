859f4a7b5be52d7e8134381fd88339db2b401c4b
Morteza Jamali <mortezajamali4241@gmail.com>
2022-09-15 07:44:11 +0000

    chore(packages): create `Get-MD5Hash()` function in `installer` package

--- packages/installer/installer.ps1
+++ packages/installer/installer.ps1
@@ -1,3 +1,15 @@
+function Get-MD5Hash($string) {
+    $md5 = New-Object -TypeName System.Security.Cryptography.MD5CryptoServiceProvider;
+    $utf8 = New-Object -TypeName System.Text.UTF8Encoding;
+    $hash = [System.BitConverter]::ToString($md5.ComputeHash($utf8.GetBytes($string)))
+
+    return $hash;
+}
+
+function Run_PWSH($command) {
+    pwsh -NoLogo -STA -NonInteractive -NoProfile -ExecutionPolicy Unrestricted -Command $command
+}
+
 function Check_Program_Installed($programName) {
     $x86_check = ((Get-ChildItem "HKLM:Software\Microsoft\Windows\CurrentVersion\Uninstall") |
             Where-Object { $_.Name -like "*$programName*" }).Length -gt 0;
@@ -10,23 +22,38 @@ function Check_Program_Installed($programName) {
     return $x86_check -or $x64_check;
 }
 
-if (!(Check_Program_Installed("vagrant"))) {
+function Install-Vagrant {
     Write-Host("Vagrant is not installed !");
     # TODO: install vagrant using Powershell
 }
 
-$installRunnerCommand = {
-    New-Item -Name "actions-runner" -ItemType "directory" -Force;
-    Set-Location -Path "actions-runner";
+function Install-Runner {
+    $command = {
+        $runner_path = Join-Path $env:SystemDrive "actions-runner";
 
-    $runnerAssetObject = ((Invoke-WebRequest -Uri "https://api.github.com/repos/actions/runner/releases?per_page=1" -Headers @{ "Accept" = "application/vnd.github+json" } -ContentType "application/json").Content |
-            ConvertFrom-Json).assets |
-            Where-Object { $_.name -like "*-win-x64-*" -and $_.name -notlike "*noexternals*" -and $_.name -notlike "*noruntime*" -and $_.name -notlike "*trimmedpackages*" } |
-            Select-Object -Property browser_download_url, name;
+        New-Item -Path $runner_path -ItemType "directory" -Force;
+        Set-Location -Path $runner_path;
 
-    Invoke-WebRequest -Uri $runnerAssetObject.browser_download_url -OutFile $runnerAssetObject.name;
-    Add-Type -AssemblyName System.IO.Compression.FileSystem;
-    [System.IO.Compression.ZipFile]::ExtractToDirectory("$PWD\$($runnerAssetObject.name)", "$PWD");
+        $request_uri = "https://api.github.com/repos/actions/runner/releases?per_page=1";
+        $request_headers = @{
+            "Accept" = "application/vnd.github+json";
+            "ContentType" = "application/json";
+        };
+        $request_asset_object = ((Invoke-WebRequest -Uri $request_uri -Headers $request_headers).Content |
+                ConvertFrom-Json).assets |
+                Where-Object { $_.name -like "*-win-x64-*" -and $_.name -notlike "*noexternals*" `
+                -and $_.name -notlike "*noruntime*" -and $_.name -notlike "*trimmedpackages*" } |
+                Select-Object -Property "browser_download_url", "name";
+
+        Invoke-WebRequest -Uri $request_asset_object.browser_download_url -OutFile $request_asset_object.name;
+        Add-Type -AssemblyName System.IO.Compression.FileSystem;
+        [System.IO.Compression.ZipFile]::ExtractToDirectory((Join-Path "$PWD" $request_asset_object.name), "$PWD");
+    }
+
+    Run_PWSH($command);
 }
 
-pwsh -NoLogo -STA -NonInteractive -NoProfile -ExecutionPolicy Unrestricted -WorkingDirectory "${env:SystemDrive}\" -Command $installRunnerCommand
+if (-Not (Check_Program_Installed("vagrant"))) {
+    Install-Vagrant
+}
+
