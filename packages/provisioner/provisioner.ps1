6a1046217b61f0c154593273c02399db87104572
Morteza Jamali <mortezajamali4241@gmail.com>
2022-09-16 21:42:13 +0000

    chore(packages): create `Invoke-GHJob` function in `provisioner.ps1` script

--- packages/provisioner/provisioner.ps1
+++ packages/provisioner/provisioner.ps1
@@ -100,10 +100,25 @@ process {
     };
   }
 
-  $jobs_url = (Get-GHWorkflowContent -RunID $_.GitHubContext.run_id `
-      -Repo $_.GitHubContext.repository -Ref $_.GitHubContext.ref).Metadata.jobs_url;
+  function Invoke-GHJob {
+    [CmdletBinding()]
+    param (
+      [Parameter(Mandatory = $true)]
+      [pscustomobject]
+      $Job
+    )
+    
+    # Invoke vagrant module
+  }
+
+  $github_context = $_.GitHubContext;
+  $jobs_url = (Get-GHWorkflowContent -RunID $github_context.run_id `
+      -Repo $github_context.repository -Ref $github_context.ref).Metadata.jobs_url;
   
   (Invoke-GHWebRequest -URI $jobs_url).ContentObject.jobs | ForEach-Object {
-    $_
+    if ($_.name -ne $github_context.job -and $_.status -ne "completed") {
+      Invoke-GHJob -Job $_;
+      return;    
+    }
   };
 }
