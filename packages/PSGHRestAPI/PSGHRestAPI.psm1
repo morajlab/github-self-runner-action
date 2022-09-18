aea61a4494a4c36e4838b74ca90c8790b187e96d
Morteza Jamali <mortezajamali4241@gmail.com>
2022-09-18 19:03:02 +0000

    feat(packages): create `PSGHRestAPI`, `PSGHSelfRunner` and `PSVagrantHTTPServer` powershell module

---
+++ packages/PSGHRestAPI/PSGHRestAPI.psm1
@@ -0,0 +1,90 @@
+  Import-Module powershell-yaml
+
+  function Invoke-GHWebRequest {
+    [CmdletBinding()]
+    param (
+      [Parameter(Mandatory = $true)]
+      [string]
+      $URI,
+      [Parameter(Mandatory = $false)]
+      [pscustomobject]
+      $Headers,
+      [Parameter(Mandatory = $false)]
+      [string]
+      $Token
+    )
+
+    $all_headers = @{
+      "Accept" = "application/vnd.github+json";
+    };
+
+    if ($Headers -ne $null) {
+      $all_headers += $Headers;
+    }
+
+    if ($Token -ne $null -and $Token -ne '') {
+      $all_headers += @{ "Authorization" = "Bearer ${Token}" };
+    }
+
+    $response = Invoke-WebRequest -Uri $URI -Headers $all_headers;
+
+    return @{
+      Response      = $response;
+      ContentObject = ($response.Content | ConvertFrom-Json);
+    };
+  }
+
+  function Get-GHFileContent {
+    [CmdletBinding()]
+    param (
+      [Parameter(Mandatory = $true)]
+      [string]
+      $Repo,
+      [Parameter(Mandatory = $false)]
+      [string]
+      $Path,
+      [Parameter(Mandatory = $false)]
+      [string]
+      $Token
+    )
+
+    $request_uri = "https://api.github.com/repos/${Repo}/contents/${Path}";
+
+    $file_content = (Invoke-GHWebRequest -URI $request_uri -Token $Token).ContentObject.content;
+    $file_content = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($file_content));
+
+    return $file_content;
+  }
+
+  function Get-GHWorkflowContent {
+    [CmdletBinding()]
+    param (
+      [Parameter(Mandatory = $true)]
+      [string]
+      $RunID,
+      [Parameter(Mandatory = $true)]
+      [string]
+      $Repo,
+      [Parameter(Mandatory = $false)]
+      [string]
+      $Token,
+      [Parameter(Mandatory = $false)]
+      [string]
+      $Ref
+    )
+
+    $request_uri = "https://api.github.com/repos/${Repo}/actions/runs/${RunID}";
+    $workflow_metadata = (Invoke-GHWebRequest -URI $request_uri -Token $Token).ContentObject;
+    $path = $workflow_metadata.path;
+
+    if ($Ref -ne $null -and $Ref -ne '') {
+      $path += "?ref=${Ref}";
+    }
+
+    $content = ConvertFrom-Yaml (Get-GHFileContent -Repo $Repo -Token $Token -Path $path);
+
+    return @{
+      Metadata = $workflow_metadata;
+      Content  = $content;
+    };
+  }
--- packages/provisioner/provisioner.ps1
+++ packages/provisioner/provisioner.ps1
@@ -9,97 +9,6 @@ param (
 )
 
 process {
-  Import-Module powershell-yaml
-
-  function Invoke-GHWebRequest {
-    [CmdletBinding()]
-    param (
-      [Parameter(Mandatory = $true)]
-      [string]
-      $URI,
-      [Parameter(Mandatory = $false)]
-      [pscustomobject]
-      $Headers,
-      [Parameter(Mandatory = $false)]
-      [string]
-      $Token
-    )
-    
-    $all_headers = @{
-      "Accept" = "application/vnd.github+json";
-    };
-
-    if ($Headers -ne $null) {
-      $all_headers += $Headers;
-    }
-
-    if ($Token -ne $null -and $Token -ne '') {
-      $all_headers += @{ "Authorization" = "Bearer ${Token}" };
-    }
-
-    $response = Invoke-WebRequest -Uri $URI -Headers $all_headers;
-
-    return @{
-      Response      = $response;
-      ContentObject = ($response.Content | ConvertFrom-Json);
-    };
-  }
-
-  function Get-GHFileContent {
-    [CmdletBinding()]
-    param (
-      [Parameter(Mandatory = $true)]
-      [string]
-      $Repo,
-      [Parameter(Mandatory = $false)]
-      [string]
-      $Path,
-      [Parameter(Mandatory = $false)]
-      [string]
-      $Token
-    )
-
-    $request_uri = "https://api.github.com/repos/${Repo}/contents/${Path}";
-
-    $file_content = (Invoke-GHWebRequest -URI $request_uri -Token $Token).ContentObject.content;
-    $file_content = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($file_content));
-
-    return $file_content;
-  }
-
-  function Get-GHWorkflowContent {
-    [CmdletBinding()]
-    param (
-      [Parameter(Mandatory = $true)]
-      [string]
-      $RunID,
-      [Parameter(Mandatory = $true)]
-      [string]
-      $Repo,
-      [Parameter(Mandatory = $false)]
-      [string]
-      $Token,
-      [Parameter(Mandatory = $false)]
-      [string]
-      $Ref
-    )
-
-    $request_uri = "https://api.github.com/repos/${Repo}/actions/runs/${RunID}";
-    $workflow_metadata = (Invoke-GHWebRequest -URI $request_uri -Token $Token).ContentObject;
-    $path = $workflow_metadata.path;
-
-    if ($Ref -ne $null -and $Ref -ne '') {
-      $path += "?ref=${Ref}";
-    }
-
-    $content = ConvertFrom-Yaml (Get-GHFileContent -Repo $Repo -Token $Token -Path $path);
-    
-    return @{
-      Metadata = $workflow_metadata;
-      Content  = $content;
-    };
-  }
-
   function Install-VagrantVM {
     # TODO: Install GitHub-hosted VMs
     vagrant init gusztavvargadr/windows-server
@@ -113,18 +22,18 @@ process {
       [pscustomobject]
       $Job
     )
-    
+
     # Invoke vagrant module
   }
 
   $github_context = $_.GitHubContext;
   $jobs_url = (Get-GHWorkflowContent -RunID $github_context.run_id `
       -Repo $github_context.repository -Ref $github_context.ref).Metadata.jobs_url;
-  
+
   (Invoke-GHWebRequest -URI $jobs_url).ContentObject.jobs | ForEach-Object {
     if ($_.name -ne $github_context.job -and $_.status -ne "completed") {
       Invoke-GHJob -Job $_;
-      return;    
+      return;
     }
   };
 }
